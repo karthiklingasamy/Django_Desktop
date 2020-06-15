261a0dada97dd56ba0130de59070032794ca4f40
karthik <karthiklingasamay@gmail.com>
2020-06-15 12:24:57 +0800

    profile route

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 #from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
 
@@ -10,14 +10,22 @@ def register(request):
 
   if request.method == "POST":
 
-        form = UserRegisterForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}!")
-            return redirect("blog-home")
+    form = UserRegisterForm(request.POST)
+
+
+    if form.is_valid():
+
+      form.save()
+      username = form.cleaned_data.get("username")
+      messages.success(request, f"Your account has been created ! You are now able to log in")
+      return redirect("login")
   else:
 
-        form = UserRegisterForm()
+
+    form = UserRegisterForm()
   return render(request, "users/register.html", {"form": form})
+
+@login_required
+def profile(request):
+  return render(request,'users/profile.html')
